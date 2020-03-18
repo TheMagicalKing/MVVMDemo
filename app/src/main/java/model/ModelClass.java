14557814761ef78f2ad4a123e5fcd3a9b523fc12
Anders Petersen <AndP@Zealand.dk>
2020-03-18 11:56:18 +0100

    Trin for trin hjælp

--- app/src/main/java/model/ModelClass.java
+++ app/src/main/java/model/ModelClass.java
@@ -1,13 +1,15 @@
 package model;
 
-import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity; // TODO Model skal være uafhængig af Android og view
 
-public class ModelClass extends AppCompatActivity {
+public class ModelClass extends AppCompatActivity /* TODO Kun activities behøver at nedarve fra AppCompatActivity */ {
 
-    public String data;
+    public String data; // TODO Sørg for at datakernen er indkapslet
+    
+    // TODO ModelClass skal stille metoder til rådighed som viewet kan benytte, fx til set og get
 
-    @Override
-    public getView(View view){
+    @Override // TODO Når klassen ikke nedarver fra AppCompatActivtiy så bliver der ikke noget at override
+    public /* TODO Metoder skal have en returtype */ getView(View view)  {
 
     }
 
